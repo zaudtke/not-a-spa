eb0fb8d85626dcf9f860f170bba2847ba18a4a05
Al Zaudtke <zaudtke@gmail.com>
2022-03-27 14:03:55 -0400

    tool setups

--- src/HorrorVillains.Web/Pages/Index.cshtml.cs
+++ src/HorrorVillains.Web/Pages/Index.cshtml.cs
@@ -5,15 +5,15 @@ namespace HorrorVillains.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+	private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+	public IndexModel(ILogger<IndexModel> logger)
+	{
+		_logger = logger;
+	}
 
-    public void OnGet()
-    {
+	public void OnGet()
+	{
 
-    }
+	}
 }
--- src/HorrorVillains.Web/Program.cs
+++ src/HorrorVillains.Web/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -8,9 +8,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
