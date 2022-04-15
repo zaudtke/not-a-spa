90f88dea154170aa66fb4f0e011d22a8f049b7b8
Allen Zaudtke <zaudtke@gmail.com>
2022-04-15 15:45:12 -0400

    temp checkin - machine switch

---
+++ src/HorrorVillains.Web/Pages/Shared/Components/Demo/DemoViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HorrorVillains.Web.Pages.Shared.Components.Demo;
+
+public class DemoViewComponent : ViewComponent
+{
+
+	public IViewComponentResult Invoke()
+	{
+		return View();
+	}
+}
