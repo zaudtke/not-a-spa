18d8db7cf82227bcc417c00285d84845a5b96582
Al Zaudtke <zaudtke@gmail.com>
2022-12-27 15:31:50 -0500

    prove controller in pages folder works.

---
+++ src/HorrorVillains.Web/Pages/Employees/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HorrorVillains.Web.Pages.Employees;
+
+[Route("employees")]
+public class EmployeesController : Controller
+{
+	// GET
+	[HttpGet("test")]
+	public IActionResult Test()
+	{
+		return Ok("From Controller");
+	}
+}
--- src/HorrorVillains.Web/Program.cs
+++ src/HorrorVillains.Web/Program.cs
@@ -21,5 +21,5 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.MapControllers();
 app.Run();
